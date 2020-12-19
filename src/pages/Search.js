a851cb3e5ac1e49839b30a9f176af03937d4bbc0
w-hideki <73641870+w-hideki@users.noreply.github.com>
2020-12-19 11:27:21 +0900

    first-commit

---
+++ src/pages/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const search = () => {
+  return (
+    <Layout>
+      Search page
+    </Layout>
+  )
+}
+
+export default search
---
+++ src/pages/Top.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+const Top = () => {
+  return (
+    <Layout>
+      Top page
+    </Layout>
+  )
+}
+
+export default Top
